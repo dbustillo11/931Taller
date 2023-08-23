8afbdd675af703ef91d93bd67eb67b39d377ea87
dbustillo11 <bustillo.contacto@gmail.com>
2023-08-23 19:18:58 -0300

    Update almacenar.js

---
+++ js/almacenar.js
@@ -0,0 +1,36 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const itemInput = document.getElementById("item");
+    const addButton = document.getElementById("agregar");
+    const clearButton = document.getElementById("limpiar");
+    const contenedor = document.getElementById("contenedor");
+  
+    // Cargar los ítems almacenados previamente al cargar la página
+    const storedItems = JSON.parse(localStorage.getItem("items")) || [];
+    updateList(storedItems);
+  
+    addButton.addEventListener("click", () => {
+      const newItem = itemInput.value.trim();
+      if (newItem !== "") {
+        storedItems.push(newItem);
+        localStorage.setItem("items", JSON.stringify(storedItems));
+        updateList(storedItems);
+        itemInput.value = "";
+      }
+    });
+  
+    clearButton.addEventListener("click", () => {
+      localStorage.removeItem("items");
+      updateList([]);
+    });
+  
+    function updateList(items) {
+      contenedor.innerHTML = "";
+      items.forEach(item => {
+        const li = document.createElement("li");
+        li.classList.add("list-group-item");
+        li.innerText = item;
+        contenedor.appendChild(li);
+      });
+    }
+  });
